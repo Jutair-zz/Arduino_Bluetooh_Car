7b0173d46ae23a2d5ce658f94e9234713e80b72b
Jutair <contatojutair@gmail.com>
2015-08-07 22:54:54 -0300

    init commiting

--- arduino_bluetooh_car/arduino_bluetooh_car.ino
+++ arduino_bluetooh_car/arduino_bluetooh_car.ino
@@ -7,42 +7,42 @@ AF_DCMotor motor1(1, MOTOR12_64KHZ);
 AF_DCMotor motor2(2, MOTOR12_64KHZ);;
 void setup() {
 Serial.begin(9600); // set serial speed
-motor1.setSpeed(255);
-motor2.setSpeed(255);
+motor1.setSpeed(255); //Set motor1 speed
+motor2.setSpeed(255); //Set motor1 speed
 
 }
 
 
 void loop(){
-while (Serial.available() == 0); // do nothing if nothing sent
-int val = Serial.read() - '0'; // deduct ascii value of '0' to find numeric value of sent number
+while (Serial.available() == 0); // Do nothing if nothing sent
+int val = Serial.read() - '0'; // Deduct ascii value of '0' to find numeric value of sent number
 
 if (val == 1) { // 
-Serial.println("Motor Frente");
-drive_frente();
+Serial.println("Motor forward");
+drive_forward(); //Call forward function
 Serial.flush(); 
 }
 else if (val == 3)//
 {
-Serial.println("Para");
-direita();
+Serial.println("Motor Right!");
+right(); //Call right function
 Serial.flush(); 
 }
 if (val == 2) { // 
-Serial.println("Motor Volta");
-drive_re();
+Serial.println("Motor backward");
+drive_backward(); //Call backward function
 Serial.flush(); 
 }
 
 if (val == 4) { //
-Serial.println("Motor Volta");
-esquerda();
+Serial.println("Motor left");
+left(); //Call right function
 Serial.flush(); 
 }
 
 if (val == 5) { //
-Serial.println("Motor Volta");
-drive_stop();
+Serial.println("Motor stop!");
+drive_stop(); //Call stop function
 Serial.flush(); 
 }
 else // if not one of above command, do nothing
@@ -52,24 +52,23 @@ else // if not one of above command, do nothing
 Serial.println(val);
 Serial.flush(); // clear serial port
 }
-void drive_frente(){              //função drive_frente
+void drive_forward(){              //Motor forward function
   motor1.run(FORWARD);
   motor2.run(FORWARD);
 }
-void drive_re(){                  //função drive_re
-  motor1.run(BACKWARD);
+void drive_backward(){                  //Motor backward function
   motor2.run(BACKWARD);
 }
-void drive_stop(){                //função drive_stop
+void drive_stop(){                //Motor stop function
  motor1.run(RELEASE); 
  motor2.run(RELEASE); 
 }
 
-void direita(){
+void right(){ 			//Motor right function
   motor1.run(RELEASE);
   motor2.run(FORWARD);
 }
-void esquerda(){
+void left(){			//Motor left function
    motor1.run(FORWARD);
   motor2.run(RELEASE);
 }
